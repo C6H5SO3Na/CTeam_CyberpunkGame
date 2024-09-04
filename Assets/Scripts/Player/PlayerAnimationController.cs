f7842b6e4af8256cc5a06e6e2ae0b354a7524b1c
CI <CI@jec.ac.jp>
2024-09-04 15:22:52 +0900

    ステージを3分割にする途中

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerAnimationController : MonoBehaviour
 {
@@ -13,6 +14,35 @@ public class PlayerAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isRun;
         animator.SetBool("Run", Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f);
+        isRun = animator.GetBool("Run");
+
+        //仮モーションテスト
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Attack2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Damage");
+        }
+
+        //アニメーション速度を調整（アニメーション名で判別）
+        string anim_name = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        if (anim_name.Contains("Run") && isRun)
+        {
+
+        }
+        else
+        {
+            animator.speed = 1.0f;
+        }
     }
 }
